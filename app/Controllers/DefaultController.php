483f8f32614055e70ea2d41a2dac6db42ad465e2
James Solomon <solomonjames@spotify.com>
2015-02-20 16:12:52 -0500

    Some small syntax changes

--- app/Controllers/DefaultController.php
+++ app/Controllers/DefaultController.php
@@ -3,21 +3,27 @@
 namespace Controllers;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Models\User;
 
 class DefaultController
 {
-
     private $app;
 
-    // constructor
+    /**
+     * Constructor
+     */
     public function __construct($app)
     {
         $this->app = $app;
     }
 
-    // Example default action that gets a custom service
-    // then renders a twig template
+    /**
+     * Example default action that gets a custom service
+     * then renders a twig template
+     *
+     * @return Response
+     */
     public function indexAction()
     {
         $message = $this->app['example_service']->getWelcomeMessage();
@@ -26,5 +32,4 @@ class DefaultController
             'message' => $message,
         ));
     }
-
 }
